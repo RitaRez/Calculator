565189eb75f382d47296219e3ee48437121c1a90
RitaRez <ritarezborges@gmail.com>
2018-03-10 14:32:50 -0300

    Ta funcionando a base de migue

--- myScript.js
+++ myScript.js
@@ -2,18 +2,6 @@ function changeOutput(char) {
     if(char != '=' )
         document.getElementById("output").innerHTML += char;
     else{
-        for(var i = 1; i < char.length(); i++){
-            aux(char[i], char[i-1]);
-        }
+        document.getElementById('output').innerHTML = eval(document.getElementById('output').innerHTML);
     }
-}
-
-function aux (i, j){
-    if(i == "+" || i == "-")
-        i = 0;
-    else if (i == "/" || i == "x")
-        return 1;
-    else if (i == "sqrt" || i == "^")
-    else if (i == "(" || i == ")")
-        return 3;
 }
