e581095876f1608be6c35a3904008f3606e1be26
RitaRez <ritarezborges@gmail.com>
2018-03-18 12:20:52 -0300

    Added clear button

--- myScript.js
+++ myScript.js
@@ -1,7 +1,9 @@
 function changeOutput(char) {
-    if(char != '=' )
+    if(char != '=' && char != "clear")
         document.getElementById("output").innerHTML += char;
-    else{
+    else if(char == "="){
         document.getElementById('output').innerHTML = eval(document.getElementById('output').innerHTML);
+    } else{
+        document.getElementById('output').innerHTML = "";
     }
 }
